fd5d36fbf60dab5bbe06946bb8359f20a6ad3e7e
DocTor894 <maksim.chernyh.01@gmail.com>
2020-07-28 15:40:49 +1000

    triangle header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home__element" v-for="el in elements">
+      <router-link :to="el.src">{{ el.title }}</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+
+  },
+  data() {
+    return {
+      elements: {
+        first: {
+          title: 'TrHeader',
+          src: '/trHeader'
+        }
+      }
+    };
   },
 };
 </script>
---
+++ src/views/TriangleHeader/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <header>
+    <div class="header__left">
+      <div class="header__tgl-first"></div>
+      <div class="header__tgl-second"></div>
+    </div>
+    <div class="header__right">
+      <div class="header__tgl-thread"></div>
+      <div class="header__tgl-four"></div>
+    </div>
+    <div class="header__content">
+      <p class="block">Content!!!</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TriangleHeader',
+};
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+header {
+  background: #ffffff;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
+
+.header__left {
+  float: left;
+  /* background: #808080; */
+  height: 100%;
+  width: 40%;
+  position: relative;
+}
+
+.header__tgl-first {
+  width: 800px;
+  height: 1400px;
+  background: #60b8e6;
+  transform: rotateZ(-30deg);
+  position: absolute;
+  right: 226px;
+  bottom: -294px;
+  box-shadow: 0 0 30px rgba(0,0,0,0.5);
+  /* z-index: 300; */
+}
+
+.header__tgl-second {
+  width: 800px;
+  height: 1400px;
+  background: #297fb8;
+  transform: rotateZ(-30deg);
+  position: absolute;
+  right: 297px;
+  bottom: -294px;
+  box-shadow: 0 0 30px rgba(0,0,0,0.5);
+  z-index: 300;
+}
+
+.header__right {
+  float: right;
+  /* background: #505050; */
+  height: 100%;
+  width: 60%;
+  position: relative;
+  right: 0;
+}
+
+.header__tgl-thread {
+  width: 800px;
+  height: 2000px;
+  background: #8d44ad;
+  transform: rotateZ(50deg);
+  position: absolute;
+  left: 500px;
+  bottom: -662px;
+  box-shadow: 0 0 30px rgba(0,0,0,0.5);
+  z-index: 200;
+}
+
+.header__tgl-four {
+  width: 800px;
+  height: 2000px;
+  background: #9c55bd;
+  transform: rotateZ(50deg);
+  position: absolute;
+  left: 230px;
+  bottom: -662px;
+  box-shadow: 0 0 30px rgba(0,0,0,0.5);
+  z-index: 100;
+}
+
+.header__content {
+  position: absolute;
+  display: flex;
+  right: 0;
+  left: 0;
+  width: 1200px;
+  height: 100%;
+  margin: auto;
+  /* background: rgba(10,10,10, 0.5); */
+  z-index: 800;
+  text-align: center;
+}
+
+.block {
+  margin: auto;
+  color: #000000;
+  font-size: 50px;
+}
+</style>
