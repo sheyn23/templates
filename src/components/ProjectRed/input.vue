a3222dd52691c2a98d7c501d4a1c5d41f0f326f1
DocTor894 <maksim.chernyh.01@gmail.com>
2020-08-04 11:44:53 +1000

    auto size input

--- src/components/ProjectRed/input.vue
+++ src/components/ProjectRed/input.vue
@@ -13,11 +13,12 @@ export default {
 </script>
 
 <style lang="sass">
-  $inputHeight: 22px
-  $borderWidth: 1px
-  $after: ($borderWidth * 2 + $inputHeight) / 2
+
+  $inputHeight: 28px // Четные
+  $borderWidth: 6px // Нечетные
   $borderColor: darkred
   $innerColor: white
+  $bordercount: $inputHeight/2 + $borderWidth
 
   .input
     background: $innerColor
@@ -27,36 +28,38 @@ export default {
     position: relative
 
     .input__left
-      &::before, &::after
-        content: ''
-        position: absolute
-        left: (-$inputHeight - 5px)
-        bottom: -1px
-        border: $after solid transparent
-        border-right: $after+2 solid $borderColor
-
       &::after
-        border-right: $after+1 solid $innerColor
-        bottom: -1px
-        left: (-$inputHeight - 3px)
-
-
-    .input__right
-      &::before, &::after
         content: ''
         position: absolute
-        right: (-$inputHeight - 5px)
-        bottom: -1px
-        border: $after solid transparent
-        border-left: $after+2 solid $borderColor
+        bottom: 0
+        left: (-$inputHeight - $borderWidth/2)
+        border: $inputHeight/2 solid transparent
+        border-right: $inputHeight/2 + $borderWidth/2 solid $innerColor
 
+      &::before
+        content: ''
+        position: absolute
+        bottom: -$borderWidth
+        left: (-$inputHeight - $borderWidth*3)
+        border: $bordercount solid transparent
+        border-right: $bordercount solid $borderColor
 
     .input__right
       &::after
-        border-left: $after+1 solid $innerColor
-        bottom: -1px
-        right: (-$inputHeight - 3px)
+        content: ''
+        position: absolute
+        bottom: 0
+        right: (-$inputHeight - $borderWidth/2)
+        border: $inputHeight/2 solid transparent
+        border-left: $inputHeight/2 + $borderWidth/2 solid $innerColor
 
+      &::before
+        content: ''
+        position: absolute
+        bottom: -$borderWidth
+        right: (-$inputHeight - $borderWidth*3)
+        border: $bordercount solid transparent
+        border-left: $bordercount solid $borderColor
 
     input
       border: none
