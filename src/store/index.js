8ccdedc87d5bce2d6717a8e942bd1417620f57f4
DocTor894 <maksim.chernyh.01@gmail.com>
2020-08-22 21:41:14 +1000

    add points

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import region from './modules/region.js'
+import region from './modules/region'
+import point from './modules/point'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    region
+    region,
+    point,
   },
 });
---
+++ src/store/modules/point.js
@@ -0,0 +1,42 @@
+const state = {
+  point: []
+};
+
+const getters = {
+  getPoint: (state) => {
+    return state.point[0] ? state.point[0] : null
+  },
+};
+
+const mutations = {
+  setTitle: (state, title) => {
+    state.point[0].title = title;
+    console.log(state)
+  },
+  setPosition: (state, pos) => {
+    state.point.push(pos);
+  },
+  closePoint: (state) => {
+    state.point = [];
+  },
+};
+
+const actions = {
+  setTitle({ commit, state }, title) {
+    commit('setTitle', title);
+  },
+  setPosition({ commit, state }, pos) {
+    commit('setPosition', pos);
+  },
+  closePoint({ commit, state }) {
+    commit('closePoint');
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
